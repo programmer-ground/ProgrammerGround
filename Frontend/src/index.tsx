0738bb50ae423ca0ccfd29009380184ab89a1c5f
thdwlsgus0 <thdwlsgus0@naver.com>
2021-01-08 02:35:13 +0900

    modify - 상대경로 -> 절대경로로 변환

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginPage from '../src/pages/LoginPage';
+import LoginPage from '@src/pages/LoginPage';
 ReactDOM.render(
    <LoginPage/>,
    document.getElementById('root')
