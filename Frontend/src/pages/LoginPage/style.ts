33cb076eb30aca6b8b7b9ca676cf18c3a8db82fc
thdwlsgus0 <thdwlsgus0@naver.com>
2021-01-12 12:22:25 +0900

    style - 반응형 웹 만들기 : 'media-query를 이용해서 반응형 웹 생성'

--- Frontend/src/pages/LoginPage/style.ts
+++ Frontend/src/pages/LoginPage/style.ts
@@ -18,6 +18,12 @@ export const LoginContainer=styled.div`
   border:1px solid black;
   background-color:#fff;
   margin:200px auto;
+  @media screen  and (max-width: 840px) and (min-width:480px){
+    height:330px;
+  }
+  @media screen  and (max-width: 479px) and (min-width:230px){
+    height:300px;
+  }
 `;
 
 export const LoginLogo = styled.img.attrs({
@@ -27,6 +33,19 @@ export const LoginLogo = styled.img.attrs({
   width:300px;
   height:250px;
   margin:0 auto;
+  
+  @media screen and (max-width: 840px){
+    height:200px;
+    width:200px;
+  }
+  @media screen  and (max-width: 840px) and (min-width:480px){
+    height:200px;
+    width:200px;
+  }
+  @media screen  and (max-width: 479px) and (min-width:230px){
+    height:130px;
+    width:130px;
+  }
 `;
 export const LoginButtonContainer = styled.div`
    width:100%;
@@ -46,11 +65,28 @@ export const LoginButton = styled.button`
   &:hover{
     cursor:pointer;
   }
+
+@media screen  and (max-width: 840px) and (min-width:480px){
+  height:60px;
+  width:340px;
+  font-size:20px;
+}
+@media screen  and (max-width: 479px) and (min-width:230px){
+  height:40px;
+  width:260px;
+  font-size:10px;
+}
+
 `;
+
 export const GithubLogo = styled.img.attrs({
  src:githublogo
 })`
   width:25px;
   height:25px;
   margin-left:10px; 
+  @media screen  and (max-width: 479px) and (min-width:230px){
+    height:15px;
+    width:15px;
+  }
 `;
