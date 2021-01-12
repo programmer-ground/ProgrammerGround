fb7ca455a89c2ecceb387a20c10e7bf0f35d5027
thdwlsgus0 <thdwlsgus0@naver.com>
2021-01-12 18:01:35 +0900

    babel ie9까지 지원

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,58 +1,59 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry:'./src/index.tsx',
-  output:{
-    filename:'bundle.js',
-    path:path.resolve(__dirname, 'dist'),
-    publicPath:'/'
+  entry: ['@babel/polyfill', './src/index.tsx'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
-  mode:'none',
-  resolve:{
-    extensions:['.js','.jsx','.ts', '.tsx'],
-    alias:{
-      '@src':path.resolve(__dirname, 'src')
-    }
+  mode: 'none',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.css$/i,
-        use:['style-loader', 'css-loader'],
-        exclude:/node_modules/,
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
       },
       {
-        test:/\.jsx?$/i,
-        use:['babel-loader'],
-        exclude:/node_modules/
+        test: /\.jsx?$/i,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
       },
       {
-        test:/\.tsx?$/i,
-        use:['ts-loader'],
-        exclude:/node_modules/
+        test: /\.tsx?$/i,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
       },
       {
-        test:/\.(png|jpg|svg)$/,
-        use:['file-loader'],
-        exclude:/node_modules/
-      }
-    ]
+        test: /\.(png|jpg|svg)$/,
+        use: ['file-loader'],
+        exclude: /node_modules/,
+      },
+    ],
   },
-  devServer:{
-    contentBase:path.join(__dirname, "dist"),
-    host:'localhost',
-    publicPath:'/',
-    overlay:true,
-    stats:'errors-only',
-    port:3000,
-    historyApiFallback:true
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    host: 'localhost',
+    publicPath: '/',
+    overlay: true,
+    stats: 'errors-only',
+    port: 3000,
+    historyApiFallback: true,
   },
-  plugins:[
+  plugins: [
     new HtmlWebPackPlugin({
-      template:'./public/index.html'
+      template: './public/index.html',
     }),
     new CleanWebpackPlugin(),
-  ]
-}
+  ],
+};
