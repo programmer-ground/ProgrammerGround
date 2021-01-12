a2e076c90d202b564df59e4f6b024098f34f26d6
thdwlsgus0 <thdwlsgus0@naver.com>
2021-01-12 13:39:37 +0900

    eslint 설정

---
+++ Frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'airbnb-base', // or airbnb-base
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended', // 설치 한경우
+    'plugin:import/errors', // 설치한 경우
+    'plugin:import/warnings', // 설치한 경우
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 1,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
--- Frontend/src/pages/LoginPage/index.tsx
+++ Frontend/src/pages/LoginPage/index.tsx
@@ -1,24 +1,23 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable import/extensions */
 import React from 'react';
 import * as StyledComponent from './style';
 
-const LoginPage = () =>{
-
-  return(
+const LoginPage = () => {
+  return (
     <>
-    <StyledComponent.GlobalStyle/>
-    <StyledComponent.LoginContainer>
-          <StyledComponent.LoginLogo/>
-          <StyledComponent.LoginButtonContainer>
-            <StyledComponent.LoginButton>
-              Sign In With GitHub
-              <StyledComponent.GithubLogo/>
-            </StyledComponent.LoginButton>
-         
-          </StyledComponent.LoginButtonContainer>
-    </StyledComponent.LoginContainer>
+      <StyledComponent.GlobalStyle />
+      <StyledComponent.LoginContainer>
+        <StyledComponent.LoginLogo />
+        <StyledComponent.LoginButtonContainer>
+          <StyledComponent.LoginButton>
+            Sign In With GitHub
+            <StyledComponent.GithubLogo />
+          </StyledComponent.LoginButton>
+        </StyledComponent.LoginButtonContainer>
+      </StyledComponent.LoginContainer>
     </>
-
-  )
-}
+  );
+};
 
 export default LoginPage;
