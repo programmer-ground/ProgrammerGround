fc128608d68b84e5adc4ccbf29264253152d4346
DESKTOP-JTO2FCJ\cjw72 <cjw7242@gmail.com>
2021-01-22 22:04:46 +0900

    CrossOrigin

--- Authentication/src/main/java/com/pg/auth/controller/AuthController.java
+++ Authentication/src/main/java/com/pg/auth/controller/AuthController.java
@@ -9,16 +9,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class AuthController {
     private final UserService userService;
 
