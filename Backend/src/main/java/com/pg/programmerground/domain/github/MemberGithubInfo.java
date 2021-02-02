8fc8ad2268c5c7261493c111c99829190a0ceb26
DESKTOP-E1B1NK8\user <cjw7242@gmail.com>
2021-02-02 14:53:04 +0900

    refactor: Entity 수정

---
+++ Backend/src/main/java/com/pg/programmerground/domain/github/MemberGithubInfo.java
@@ -0,0 +1,43 @@
+package com.pg.programmerground.domain.github;
+
+import com.pg.programmerground.domain.OAuthMember;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
+public class MemberGithubInfo {
+
+  @Id
+  @Column(name = "MEMBER_GITHUB_INFO_ID")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToOne(mappedBy = "memberGithubInfo", cascade = CascadeType.ALL, orphanRemoval = true)
+  private OAuthMember member;
+
+  private int commitCnt;
+  private int pullRequestCnt;
+  private String mostLanguage;
+  private int repositoryCnt;
+  private String githubPage;
+  private String profileImg;
+
+  /*@OneToMany(mappedBy = "memberGithubInfo", cascade = CascadeType.ALL, orphanRemoval = true)
+  private final List<GithubRepositoryInfo> githubRepositories = new ArrayList<>();
+
+  public void addGithubRepository(GithubRepositoryInfo githubRepositoryInfo) {
+      githubRepositories.add(githubRepositoryInfo);
+  }*/
+
+  public void updateInfo(MemberGithubInfo memberGithubInfo) {
+    this.commitCnt = memberGithubInfo.commitCnt;
+    this.pullRequestCnt = memberGithubInfo.pullRequestCnt;
+    this.mostLanguage = memberGithubInfo.mostLanguage;
+    this.repositoryCnt = memberGithubInfo.repositoryCnt;
+  }
+}
