55949183620593d0b1bc64f2fecb6355589946ee
thdwlsgus0 <thdwlsgus0@naver.com>
2021-02-06 00:13:12 +0900

    style : header 이미지 추가
    1. header에 이미지를 추가하였습니다

---
+++ Frontend/src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as StyledComponent from './style';
+
+const Header = () => {
+	return (
+		<StyledComponent.HeaderContainer>
+			<StyledComponent.HeaderImg />
+		</StyledComponent.HeaderContainer>
+	);
+};
+
+export default Header;
