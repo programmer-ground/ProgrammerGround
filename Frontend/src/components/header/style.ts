b3f2bbd9d6a1334b7314bb547e806f53b769cad4
thdwlsgus0 <thdwlsgus0@naver.com>
2021-02-06 00:13:12 +0900

    style : header 이미지 추가
    1. header에 이미지를 추가하였습니다

---
+++ Frontend/src/components/header/style.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+import logo from '@src/assets/programmerground.png';
+
+export const HeaderImg = styled.img.attrs({
+	src: logo,
+})`
+	width: 400px;
+	height: 100px;
+`;
