9b9903ea0a2b9853871123a95f776d6b44a61ddd
thdwlsgus0 <thdwlsgus0@naver.com>
2021-02-11 12:59:45 +0900

    style : toggle하기 위한 객체 생성
    1. toggle하기 위한 객체를 생성하였습니다.

--- Frontend/src/components/Common/bone/index.tsx
+++ Frontend/src/components/Common/bone/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import * as StyledComponent from './style';
 
 const Bone = () => {
-	return <StyledComponent.BoneContainer />;
+	return (
+		<StyledComponent.BoneContainer>
+			<StyledComponent.BoneMoveContainer />
+		</StyledComponent.BoneContainer>
+	);
 };
 
 export default Bone;
--- Frontend/src/components/Common/bone/style.ts
+++ Frontend/src/components/Common/bone/style.ts
@@ -2,10 +2,21 @@
 import styled from 'styled-components';
 
 export const BoneContainer = styled.div`
+	position: relative;
 	width: 50px;
 	height: 30px;
 	margin-top: 5px;
 	margin-bottom: 5px;
 	background-color: #d9dfe2;
 	border-radius: 50px;
+	border: 1px solid #d9dfe2;
+	cursor: pointer;
+`;
+
+export const BoneMoveContainer = styled.div`
+	width: 28px;
+	height: 30px;
+	border-radius: 50px;
+	background-color: #fff;
+	position: absolute;
 `;
