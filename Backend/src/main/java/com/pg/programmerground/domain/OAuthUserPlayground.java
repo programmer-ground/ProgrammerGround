3fb6569a7c347c6a981d61180854c438f19d523d
dailyworker <workingssu@gmail.com>
2021-02-15 20:50:49 +0900

    feat : initial UserPlayground 아주 간단한 초기 도메인 모델

---
+++ Backend/src/main/java/com/pg/programmerground/domain/OAuthUserPlayground.java
@@ -0,0 +1,64 @@
+package com.pg.programmerground.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "TBL_USER_PLAYGROUND")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OAuthUserPlayground {
+
+  @Id
+  @Column(name = "USER_PLAYGROUND_ID")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "OAUTH_USER_ID")
+  private OAuthUser leader;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "PLAYGROUND_ID")
+  private Playground playground;
+
+  public static OAuthUserPlayground createOAuthUserPlayground(OAuthUser oAuthUser,
+      Playground playground) {
+    OAuthUserPlayground userPlayground = new OAuthUserPlayground();
+    userPlayground.leader = oAuthUser;
+    userPlayground.playground = playground;
+    return userPlayground;
+  }
+
+  public void addPlayground(Playground playground) {
+    this.playground = playground;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof OAuthUserPlayground)) {
+      return false;
+    }
+    OAuthUserPlayground that = (OAuthUserPlayground) o;
+    return Objects.equals(getId(), that.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId());
+  }
+}
