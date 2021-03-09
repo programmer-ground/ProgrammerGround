2dbfd83081b622b98c8a6411d79f30eb1f0b1712
thdwlsgus0 <thdwlsgus0@naver.com>
2021-03-10 00:40:16 +0900

    modal 상태관리 구축

---
+++ Frontend/src/components/Common/modal/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import * as StyledComponent from './style';
+
+const ModalWrapper = () => {
+	return (
+		<div>
+			<StyledComponent.Container />
+		</div>
+	);
+};
+
+export default ModalWrapper;
---
+++ Frontend/src/components/Common/modal/style.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	background-color: rgba(249, 249, 249, 0.85);
+	z-index: 100;
+`;
