288eb6f12c980bf2a7f2ec13957b03124fe28a58
thdwlsgus0 <thdwlsgus0@naver.com>
2021-03-09 23:38:47 +0900

    style: 폰트 적용

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -4,5 +4,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './root.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- Frontend/src/store/index.ts
+++ Frontend/src/store/index.ts
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer from './reducers';
-import rootSaga from './sagas';
+// import { createStore, applyMiddleware } from 'redux';
+// import createSagaMiddleware from 'redux-saga';
+// import rootReducer from './reducers';
+// import rootSaga from './sagas';
 
-const sagaMiddleware = createSagaMiddleware();
+// const sagaMiddleware = createSagaMiddleware();
 
-const configureStore = () => {
-	const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-	sagaMiddleware.run(rootSaga);
-	return store;
-};
-export default configureStore;
+// const configureStore = () => {
+// 	const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+// 	sagaMiddleware.run(rootSaga);
+// 	return store;
+// };
+// export default configureStore;
