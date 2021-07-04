5eff740f1e1b46bca7f9a37a2d4f569a01a62a0a
dailyworker <workingssu@gmail.com>
2021-07-04 18:10:11 +0900

    test : github Bot 관련 테스트 추가  - TestUserManagerment.saveGithubRepoBotUser() : 테스트 전역에서 사용하기 위한 깃허브 봇 등록  - PlaygroundBotTest : 실제 봇 유저를 활용하여 테스트 수행  - GithubBotConfigTest : yml 파일을 제대로 가져오는지 테스트 수행

--- Backend/src/test/java/com/pg/programmerground/playground/PlayGroundBotTest.java
+++ Backend/src/test/java/com/pg/programmerground/playground/PlayGroundBotTest.java
@@ -4,12 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.pg.programmerground.TestUserManagement;
-import com.pg.programmerground.model.Oauth2AuthorizedClientRepository;
+import com.pg.programmerground.auth.jwt.JwtAuthenticationToken;
+import com.pg.programmerground.config.GithubBotConfig;
+import com.pg.programmerground.domain.Playground;
+import com.pg.programmerground.dto.playground.api_req.PlaygroundApi;
+import com.pg.programmerground.model.PlaygroundRepository;
 import com.pg.programmerground.service.GithubRestService;
+import com.pg.programmerground.service.OAuthUserService;
+import com.pg.programmerground.service.PlaygroundService;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -20,6 +27,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,11 +37,12 @@ import org.springframework.web.client.RestTemplate;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PlayGroundBotTest {
 
-  private static final Long GITHUB_BOT_ID = 83174448L;
-  private static final String PREFIX_GITHUB_API = "https://api.github.com/orgs/programmer-ground";
-  private static final String PREFIX_ACCESS_TOKEN = "Bearer ";
+  private static final Long GITHUB_ID = 1234L;
   private static final RestTemplate restTemplate = new RestTemplate();
   TestJsonPackage dtoList;
+
+  @Autowired
+  private GithubBotConfig githubBotConfig;
 
   @Autowired
   private GithubRestService githubRestService;
@@ -40,13 +51,28 @@ public class PlayGroundBotTest {
   TestUserManagement management;
 
   @Autowired
-  private Oauth2AuthorizedClientRepository oauth2AuthorizedClientRepository;
+  private OAuthUserService oAuthUserService;
+
+  @Autowired
+  private PlaygroundService playgroundService;
+
+  @Autowired
+  private PlaygroundRepository playgroundRepository;
 
 
   @BeforeAll
   void dataSetUp() throws IOException {
     dtoList = TestJsonPackage.of();
+    management.saveTestUser();
     management.saveGithubRepoBotUser();
+  }
+
+  @BeforeEach
+  void authenticationSetUp() {
+    //Github 로그인을 한번 시도하고
+    UserDetails userDetails = oAuthUserService.loadUserByOAuthId(GITHUB_ID);
+    Authentication authentication = new JwtAuthenticationToken(userDetails, "토큰 값 필요없음", userDetails.getAuthorities());
+    SecurityContextHolder.getContext().setAuthentication(authentication);
   }
 
   @Test
@@ -56,9 +82,23 @@ public class PlayGroundBotTest {
     HttpHeaders headers = getHeaders();
     //when
     String rest = githubRestService
-        .rest(PREFIX_GITHUB_API, HttpMethod.GET, headers, String.class);
+        .rest(githubBotConfig.getOrgUrl(), HttpMethod.GET, headers, String.class);
     //then
     System.out.println(rest);
+  }
+
+  @Test
+  @DisplayName("PlayGround의 Leader가 Github Repository 생성 요청을 할 수 있다")
+  public void playground_leader_request_create_github_repo() throws Exception {
+    //given
+    PlaygroundApi playgroundApi = dtoList.createPlayground;
+    Long playgroundId = playgroundService.createPlayground(playgroundApi);
+    Playground playground = playgroundRepository.findById(playgroundId).orElseThrow();
+
+    //when
+    playgroundService.getPlaygroundDetailInfo(playgroundId);
+
+    //then
   }
 
   @Test
@@ -78,22 +118,62 @@ public class PlayGroundBotTest {
     HttpEntity<Map<String, String>> request = new HttpEntity<>(requestBody, getHeaders());
     //when
     ResponseEntity<String> responseEntity = restTemplate
-        .postForEntity(PREFIX_GITHUB_API + "/repos", request, String.class);
+        .postForEntity(githubBotConfig.getOrgUrl() + "/repos", request, String.class);
     //then
     assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
   }
 
+  @Test
+  @DisplayName("해당 Repository에 유저를 Collaborator로 추가할 수 있다")
+  public void add_collaborator_github_repo() {
+    //given
+    String targetUrl = githubBotConfig.getOrgRepoUrl() + "/test-repo/collaborators/pg-test-user-1";
+    HttpEntity<?> entity = new HttpEntity<>(getHeaders());
 
+    //when
+    ResponseEntity<String> exchange = restTemplate
+        .exchange(targetUrl, HttpMethod.PUT, entity, String.class);
+
+    //then
+    assertEquals(exchange.getStatusCode(), HttpStatus.CREATED);
+  }
+
+  @Test
+  @DisplayName("해당 Repository에 추가할 유저가 Collabrator로 등록이 안되어있을 시 404 익셉션이 발생한다")
+  public void if_not_exist_collaborator_github_repo() {
+    //given
+    String targetUrl = githubBotConfig.getOrgRepoUrl() +"/test-repo/collaborators/pg-test-user-1";
+    HttpEntity<?> entity = new HttpEntity<>(getHeaders());
+
+    //when
+    HttpClientErrorException thrown = assertThrows(HttpClientErrorException.class,
+        () -> restTemplate
+            .exchange(targetUrl, HttpMethod.GET, entity, String.class)
+    );
+
+    //then
+    assertEquals(thrown.getStatusCode(), HttpStatus.NOT_FOUND);
+  }
+
+  @Test
+  @DisplayName("해당 Repository에 추가할 유저가 Collabrator로 등록이 되어있을 시 204 No Content 리스폰스가 발생한다")
+  public void if_exist_collaborator_github_repo() {
+    //given
+    String targetUrl = githubBotConfig.getOrgRepoUrl() + "/test-repo/collaborators/pg-test-user-2";
+    HttpEntity<?> entity = new HttpEntity<>(getHeaders());
+
+    //when
+    ResponseEntity<String> exchange = restTemplate
+        .exchange(targetUrl, HttpMethod.GET, entity, String.class);
+
+    //then
+    assertEquals(exchange.getStatusCode(), HttpStatus.NO_CONTENT);
+  }
 
   private HttpHeaders getHeaders() {
-    String githubBotToken = oauth2AuthorizedClientRepository
-        .findById(GITHUB_BOT_ID)
-        .orElseThrow(IllegalArgumentException::new)
-        .getAccessTokenValue();
-
     HttpHeaders headers = new HttpHeaders();
     headers.set("Accept", "application/vnd.github.v3+json");
-    headers.set("Authorization", PREFIX_ACCESS_TOKEN + githubBotToken);
+    headers.set("Authorization", githubBotConfig.getToken());
     return headers;
   }
 
