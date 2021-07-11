a18e83716123266d6e36018619fb415b384c7692
thdwlsgus0 <thdwlsgus0@naver.com>
2021-07-11 19:27:41 +0900

    feat: 무한스크롤 적용

--- Frontend/src/pages/PlayGroundPage/index.tsx
+++ Frontend/src/pages/PlayGroundPage/index.tsx
@@ -20,22 +20,63 @@ import * as StyledComponent from './style';
 
 const PlayGroundPage = () => {
 	const [show, dispatch] = useShow();
+	const [infiniteData, setInfiniteData] = useState({
+		playgroundList: [],
+		itemLength: 15,
+		prevIndex: 0,
+	});
 	const [playgrounds, setPlaygrounds] = useState([]);
 	const { playgroundShow } = useSelector(
 		(state: RootState) => state.modalReducer,
 	);
+	const infiniteScroll = () => {
+		const scrollHeight = Math.max(
+			document.documentElement.scrollHeight,
+			document.body.scrollHeight,
+		);
+		const scrollTop = Math.max(
+			document.documentElement.scrollTop,
+			document.body.scrollTop,
+		);
 
+		const { clientHeight } = document.documentElement;
+
+		if (scrollTop + clientHeight >= scrollHeight) {
+			setInfiniteData({
+				...infiniteData,
+				prevIndex: infiniteData.itemLength,
+				itemLength: infiniteData.itemLength + 15,
+			});
+			fetchData();
+		}
+	};
+
+	const fetchData = async () => {
+		try {
+			const data = await getAllPlaygrounds();
+			const result = data.playground_card.slice(
+				infiniteData.prevIndex,
+				infiniteData.itemLength,
+			);
+			setInfiniteData({
+				...infiniteData,
+				playgroundList: [...infiniteData.playgroundList, ...result],
+			});
+			setPlaygrounds(infiniteData.playgroundList);
+		} catch (e) {
+			console.log(e);
+		}
+	};
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const data = await getAllPlaygrounds();
-				setPlaygrounds(data.playground_card);
-			} catch (e) {
-				console.log(e);
-			}
-		};
 		fetchData();
 	}, []);
+
+	useEffect(() => {
+		window.addEventListener('scroll', infiniteScroll, true);
+		return () => {
+			window.removeEventListener('scroll', infiniteScroll, true);
+		};
+	}, [infiniteScroll]);
 	return (
 		<>
 			<Header />
@@ -49,10 +90,10 @@ const PlayGroundPage = () => {
 					<StyledComponent.CreateLink>방 생성</StyledComponent.CreateLink>
 				</StyledComponent.SearchContainer>
 				<StyledComponent.PlayGroundContainer>
-					{playgrounds.map((v) => {
+					{infiniteData.playgroundList.map((v) => {
 						return (
 							<PlaygroundContent
-								key={v.playground_id}
+								key={Math.random() * 10000}
 								title={v.title}
 								position={v.position_list[0].position_name}
 								language={v.position_list[0].language}
