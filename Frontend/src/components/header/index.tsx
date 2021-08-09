0007d3d19729abe0cbba223e862720c956ca9678
thdwlsgus0 <thdwlsgus0@naver.com>
2021-08-09 23:33:57 +0900

    style: 개인 정보 modal 구현

--- Frontend/src/components/header/index.tsx
+++ Frontend/src/components/header/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React, { useState } from 'react';
 import * as StyledComponent from './style';
 import './headerImage.scss';
 import SearchBar from '@src/components/searchBar/index';
 
 const Header = () => {
+	const [isAlarm, setAlarm] = useState(false);
+	const [isUser, setUser] = useState(false);
+	const userClickHandler = (e: any) => {
+		setUser(!isUser);
+	};
+	const alarmClickHandler = (e: any) => {
+		console.log('alarm');
+	};
+
 	return (
 		<>
 			<StyledComponent.GlobalStyle />
@@ -14,10 +25,39 @@ const Header = () => {
 				<StyledComponent.PlaygroundSearchSection>
 					<SearchBar />
 				</StyledComponent.PlaygroundSearchSection>
-				<StyledComponent.CreateLink>방 생성</StyledComponent.CreateLink>
 				<StyledComponent.HeaderMenuContainer>
-					<div className="user_icon" />
-					<div className="alarm_icon" />
+					<button
+						type="button"
+						className="user_icon"
+						onClick={userClickHandler}
+					/>
+					{isUser && (
+						<StyledComponent.UserMenu>
+							<StyledComponent.UserItem>
+								<a href="#">
+									<i className="user_profile" />
+									<span>나의 프로필</span>
+								</a>
+							</StyledComponent.UserItem>
+							<StyledComponent.UserItem>
+								<a href="http://localhost:3000/playground">
+									<i className="user_room" />
+									<span>방 생성</span>
+								</a>
+							</StyledComponent.UserItem>
+							<StyledComponent.UserItem>
+								<a href="#">
+									<i className="user_logout" />
+									<span>로그아웃</span>
+								</a>
+							</StyledComponent.UserItem>
+						</StyledComponent.UserMenu>
+					)}
+					<button
+						type="button"
+						className="alarm_icon"
+						onClick={alarmClickHandler}
+					/>
 				</StyledComponent.HeaderMenuContainer>
 			</StyledComponent.HeaderContainer>
 		</>
