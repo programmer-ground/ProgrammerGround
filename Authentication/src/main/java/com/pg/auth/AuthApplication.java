d18c1fd8bfbe0e29f32c9b1923cdfd6ceb9a9141
CJW23 <cjw7242@gmail.com>
2021-08-11 23:01:08 +0900

    feat: pgAPI, auth 서버 eureka 연동

--- Authentication/src/main/java/com/pg/auth/AuthApplication.java
+++ Authentication/src/main/java/com/pg/auth/AuthApplication.java
@@ -2,6 +2,7 @@ package com.pg.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
--- Backend/src/main/java/com/pg/programmerground/ProgrammergroundApplication.java
+++ Backend/src/main/java/com/pg/programmerground/ProgrammergroundApplication.java
@@ -3,11 +3,13 @@ package com.pg.programmerground;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class ProgrammergroundApplication {
 
     public static void main(String[] args) {
