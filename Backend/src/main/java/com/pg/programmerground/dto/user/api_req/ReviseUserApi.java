6a653219dca2b27c43a9a0122c9ff44495909336
dailyworker <seansin@chungbuk.ac.kr>
2021-09-06 22:40:21 +0900

    fix : Notnull 처리 추가

--- Backend/src/main/java/com/pg/programmerground/dto/user/api_req/ReviseUserApi.java
+++ Backend/src/main/java/com/pg/programmerground/dto/user/api_req/ReviseUserApi.java
@@ -5,11 +5,13 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Setter
 @Getter
 public class ReviseUserApi {
     @Length(max = 5)
     @NotBlank
+    @NotNull
     private String userName;
 }
