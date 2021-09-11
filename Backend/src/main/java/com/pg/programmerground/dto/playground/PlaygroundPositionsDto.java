0a2c1325d5fc4885c581c90e54550274bd5a0cc1
dailyworker <seansin@chungbuk.ac.kr>
2021-09-11 15:11:41 +0900

    [#133] feat : PlaygroundPositionDto, PlaygroundPositionsDto
    - dto 구현

---
+++ Backend/src/main/java/com/pg/programmerground/dto/playground/PlaygroundPositionsDto.java
@@ -0,0 +1,30 @@
+package com.pg.programmerground.dto.playground;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.pg.programmerground.domain.PlaygroundPosition;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class PlaygroundPositionsDto {
+    @JsonProperty("playground_positions")
+    private final List<PlayGroundPositionDto> playgroundPositions;
+
+    public PlaygroundPositionsDto(List<PlaygroundPosition> playgroundPositions) {
+        this.playgroundPositions = playgroundPositions.stream()
+                .map(element -> {
+                    return PlayGroundPositionDto.builder()
+                            .id(element.getId())
+                            .position(element.getPosition())
+                            .maxPositionNum(element.getMaxPositionNum())
+                            .currentPositionNum(element.getCurrentPositionNum())
+                            .positionLevel(element.getPositionLevel())
+                            .fullPosition(element.isFullPosition())
+                            .createdAt(element.getCreatedAt())
+                            .modifiedAt(element.getModifiedAt())
+                            .build();
+                }).collect(Collectors.toList());
+    }
+}
