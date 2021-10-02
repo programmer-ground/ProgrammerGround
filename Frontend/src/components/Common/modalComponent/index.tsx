b571143657be9eada7adec40c19f499c25ebf352
thdwlsgus0 <thdwlsgus0@naver.com>
2021-10-02 16:33:02 +0900

    #142 repository ui 수정 및 leader가 생성한 전체 playground list 가져오기

--- Frontend/src/components/Common/modalComponent/index.tsx
+++ Frontend/src/components/Common/modalComponent/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as StyledComponent from './style';
 import useShow from '@src/hooks/useShow';
 import { repositoryModalMode } from '@src/store/modules/modal';
+import { getNoticeLeaderList } from '@src/lib/axios/playground';;
 
 interface modalProps {
   title: string;
@@ -11,11 +12,19 @@ interface modalProps {
 
 const ModalComponent:React.FC<modalProps> = ({title, itemList, state}) => {
   const [show, dispatch] = useShow();
+  const [noticeItem, setNoticeItem] = useState([]);
 
   const onClose = (e: React.MouseEvent<HTMLButtonElement>) => {
     dispatch(repositoryModalMode(!state));
   }
 
+  useEffect(() => {
+    const getData = async () => {
+      const noticeData = await getNoticeLeaderList();
+      setNoticeItem(noticeData.user_notice);
+    }
+    getData();
+  },[]);
   return (
     <>
        <StyledComponent.ModalContainer>
@@ -24,6 +33,21 @@ const ModalComponent:React.FC<modalProps> = ({title, itemList, state}) => {
              <StyledComponent.ModalTitle>{title}</StyledComponent.ModalTitle>
              <StyledComponent.ModalButton onClick={onClose}>X</StyledComponent.ModalButton>
            </StyledComponent.ModalHead>
+           <StyledComponent.ModalBody>
+           <StyledComponent.ModalBodyContainer>
+              {noticeItem.map((v,i)=> {
+                return (
+                  <>
+                    <StyledComponent.ModalContainerContent key={i}>
+                      <StyledComponent.ModalContentData>{v.playground_title}</StyledComponent.ModalContentData>
+                      <StyledComponent.ModalContentData>{v.date.toString().slice(0, 10)}</StyledComponent.ModalContentData>
+                      <StyledComponent.ModalContainerRepositoryCreateButton>생성</StyledComponent.ModalContainerRepositoryCreateButton>
+                    </StyledComponent.ModalContainerContent>
+                  </>
+                )
+              })}
+              </StyledComponent.ModalBodyContainer>
+           </StyledComponent.ModalBody>
            <StyledComponent.ModalSubmitButton>{title}</StyledComponent.ModalSubmitButton>
          </StyledComponent.ModalContent>
        </StyledComponent.ModalContainer>
--- Frontend/src/components/Common/modalComponent/style.ts
+++ Frontend/src/components/Common/modalComponent/style.ts
@@ -38,7 +38,7 @@ export const ModalBody = styled.div`
     &:not(:first-child) {
       margin-top: 20px;
     }
-    padding: 0 15px 30px;
+    padding-bottom: 30px;
 `;
 
 export const ModalButton = styled.button.attrs((props) => {
@@ -62,3 +62,53 @@ export const ModalSubmitButton = styled.button`
   border: 0;
   cursor: pointer; 
 `;
+
+export const ModalBodyContainer = styled.div`
+    border-top: 1px solid #e9e9e9;
+`;
+
+export const ModalContainerContent = styled.div`
+  display: flex;
+  padding: 15px;
+
+  &:not(:first-child) {
+    border-top: 1px solid #e9e9e9;
+  }
+
+`;
+
+export const ModalContentData = styled.span`
+  flex: 1 1 auto;
+
+  &:first-child {
+    font-size: 15px;
+  }
+
+  &:not(:first-child) {
+    display: inline-block;
+    font-size: 10px;
+    line-height: 22px;
+  }
+
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+
+`;
+
+export const ModalContainerRepositoryCreateButton = styled.button.attrs((props) => {
+  type:'button'
+})`
+  border: 1px solid #e9e9e9;
+  font-size: 13px;
+  line-height: 28px;
+  color: #333;
+  cursor: pointer;
+  background-color: #fff;
+
+  &:hover {
+    background-color: #78ffe0;
+    color: #fff;
+  }
+`;
+
