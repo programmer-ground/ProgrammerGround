b5172b284f660c279de98f3e3bc19030756f5045
CJW23 <cjw7242@gmail.com>
2021-11-06 23:11:33 +0900

    feat: config 서버s

---
+++ Cloud/ProgrammerGround-Config/src/test/java/com/pg/config/ProgrammerGroundConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammerGroundConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
