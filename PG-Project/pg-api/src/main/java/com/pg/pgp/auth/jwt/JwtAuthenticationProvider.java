73d033fe4c845d33ec718a76db29d9529565544b
CJW23 <cjw7242@gmail.com>
2022-01-09 16:34:41 +0900

    feat: #183 api 에러 수정

--- PG-Project/pg-api/src/main/java/com/pg/pgp/auth/jwt/JwtAuthenticationProvider.java
+++ PG-Project/pg-api/src/main/java/com/pg/pgp/auth/jwt/JwtAuthenticationProvider.java
@@ -3,6 +3,7 @@ package com.pg.pgp.auth.jwt;
 import com.pg.pgp.authentication.JwtTokenProvider;
 import com.pg.pgp.exceptions.JwtExpiredException;
 import com.pg.pgp.exceptions.OAuthUserNotFoundException;
+import com.pg.pgp.service.OAuthUserService;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -19,7 +20,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class JwtAuthenticationProvider implements AuthenticationProvider {
     private final JwtTokenProvider jwtTokenProvider;
-    private final com.pg.pgp.service.OAuthUserService OAuthUserService;
+    private final OAuthUserService oAuthUserService;
 
     /**
      * 실질적인 인증 로직 처리
@@ -29,7 +30,7 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
         try {
             String jwtToken = (String) authentication.getCredentials();
             Long OAuthId = jwtTokenProvider.getOAuthIdByToken(jwtToken);
-            UserDetails userDetails = OAuthUserService.loadUserByOAuthId(OAuthId);
+            UserDetails userDetails = oAuthUserService.loadUserByOAuthId(OAuthId);
             return new JwtAuthenticationToken(userDetails, jwtToken, userDetails.getAuthorities());
         } catch (ExpiredJwtException e) {
             throw new JwtExpiredException("토큰 만료");
